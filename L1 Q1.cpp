16d71aa85fb4f4445ea89e6be319ef061448b558
fshanom <35174505+fshanom@users.noreply.github.com>
2019-07-19 01:35:56 -0300

    Add files via upload

---
+++ L1 Q1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+/*QUESTÃO 1
+1. Faça um programa que leia três números inteiros que serão fornecidos pelo usuário, calcule e exiba a
+soma e a média dos números. Este programa deverá utilizar as seguintes funções:
+a) calcula_soma: recebe como parâmetros os 3 números inteiros, calcula e retorna a soma.
+b) calcula_media: recebe como parâmetros os 3 números inteiros, calcula e exibe a média.
+*/
+
+int calcula_soma(int numero1,int numero2,int numero3)
+{
+	int somanum;
+	somanum=numero1+numero2+numero3;
+	return somanum;
+}
+
+void calcula_media(int numero1,int numero2,int numero3)
+{
+	int somanum;
+	float medianum;
+	somanum=calcula_soma(numero1,numero2,numero3);
+	medianum=(float)somanum/3;
+	cout<<"media dos 3 numeros: "<<medianum<<endl;
+}
+
+int main ()
+{
+	int numero1,numero2,numero3,soma;
+	cout<<"digite 3 números"<<endl;
+	cin>>numero1>>numero2>>numero3;
+	soma=calcula_soma(numero1,numero2,numero3);
+	cout<<"soma: "<<soma<<endl;
+	calcula_media(numero1,numero2,numero3);
+	return 0;
+}
